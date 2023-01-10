57a3f8f111118b2a02dcf1b4c7365ce4808a1ab6
ajibade.mus <ajibade.musa@fcmb.com>
2023-01-10 12:25:49 +0100

    completed and tested new spring security version

---
+++ src/main/java/com/musdon/securityupdate/SecurityUpdateApplication.java
@@ -0,0 +1,13 @@
+package com.musdon.securityupdate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityUpdateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityUpdateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/musdon/securityupdate/SecurityUpdateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musdon.securityupdate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityUpdateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
